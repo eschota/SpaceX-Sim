ede88cf2188915f13d2d455587fbaacf8d405003
unlim3d <eschota@gmail.com>
2020-12-08 21:01:51 +0700

    UnitEco

---
+++ Assets/Engine/Scripts/UnitEco.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class UnitEco : Unit
+{
+    public CountrySO Country;
+    public bool EcoBilledFirstMonth=false;
+}
--- Assets/Engine/Scripts/UnitResearchLab.cs
+++ Assets/Engine/Scripts/UnitResearchLab.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnitResearchLab : Unit
+public class UnitResearchLab : UnitEco
 {
    
 }
