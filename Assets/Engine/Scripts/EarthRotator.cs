cc0cb41193b57204b0103f8e60f7f489454ec308
unlim3d <eschota@gmail.com>
2020-12-09 12:04:05 +0700

    ротатор со скоростью и осью вращения

--- Assets/Engine/Scripts/EarthRotator.cs
+++ Assets/Engine/Scripts/EarthRotator.cs
@@ -4,17 +4,13 @@ using UnityEngine;
 
 public class EarthRotator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    [SerializeField] float speed = 1;
+    [SerializeField] Vector3 Axis = Vector3.back;
     // Update is called once per frame
     void Update()
     {
-        if (GameManager.CurrentState == GameManager.State.MenuStartGame) transform.Rotate(Vector3.back, Time.deltaTime);
+        if (GameManager.CurrentState == GameManager.State.MenuStartGame) transform.Rotate(Axis, Time.deltaTime*speed);
 
-        if (GameManager.CurrentState == GameManager.State.Play) transform.Rotate(Vector3.back, Time.deltaTime);
+        if (GameManager.CurrentState == GameManager.State.Play) transform.Rotate(Axis, Time.deltaTime*speed);
     }
 }
