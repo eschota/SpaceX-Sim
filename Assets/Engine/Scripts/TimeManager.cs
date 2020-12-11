907885c52e3a6a0d76cfc7b077669eb1aa44081e
Vladislav <vladislav.lifirenko@gmai.com>
2020-12-11 17:52:56 +0200

    fix TimeManager

--- Assets/Engine/Scripts/TimeManager.cs
+++ Assets/Engine/Scripts/TimeManager.cs
@@ -89,63 +89,63 @@ public class TimeManager : MonoBehaviour
 
         if (days > 0 && days <= 31)
         {
-            Days = Mathf.RoundToInt(days + 1);
+            Days = (int) (days + 1);
             Months = 1;
         }
         else if (days > 31 && days <= 60)
         {
             Months = 2;
-            Days = Mathf.RoundToInt(days - 31);
+            Days = (int) (days - 31);
         }
         else if (days > 59 && days <= 90)
         {
             Months = 3;
-            Days = Mathf.RoundToInt(days - 59);
+            Days = (int) (days - 59);
         }
         else if (days > 90 && days <= 121)
         {
             Months = 4;
-            Days = Mathf.RoundToInt(days - 90);
+            Days = (int) (days - 90);
         }
         else if (days > 120 && days <= 150)
         {
             Months = 5;
-            Days = Mathf.RoundToInt(days - 120);
+            Days = (int) (days - 120);
         }
         else if (days > 151 && days <= 181)
         {
             Months = 6;
-            Days = Mathf.RoundToInt(days - 151);
+            Days = (int) (days - 151);
         }
         else if (days > 181 && days <= 212)
         {
             Months = 7;
-            Days = Mathf.RoundToInt(days - 181);
+            Days = (int) (days - 181);
         }
         else if (days > 212 && days <= 243)
         {
             Months = 8;
-            Days = Mathf.RoundToInt(days - 212);
+            Days = (int) (days - 212);
         }
         else if (days > 243 && days <= 273)
         {
             Months = 9;
-            Days = Mathf.RoundToInt(days - 242);
+            Days = (int) (days - 242);
         }
         else if (days > 273 && days <= 304)
         {
             Months = 10;
-            Days = Mathf.RoundToInt(days - 273);
+            Days = (int) (days - 273);
         }
         else if (days > 304 && days <= 334)
         {
             Months = 11;
-            Days = Mathf.RoundToInt(days - 303);
+            Days = (int) (days - 303);
         }
         else if (days > 334 && days <= 365)
         {
             Months = 12;
-            Days = Mathf.RoundToInt(days - 333);
+            Days = (int) (days - 333);
         }
     }
 }
