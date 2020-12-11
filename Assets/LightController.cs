1f2ef56c5ce45fcb5c9b608a71d79563dc9dde80
Kos Kokos <eschota@gmail.com>
2020-12-11 23:47:03 +0700

    вращение на хую солнца

--- Assets/LightController.cs
+++ Assets/LightController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class LightController : MonoBehaviour
 {
+    [SerializeField] Light Sun;
+    [SerializeField] AnimationCurve SunIntensity;
+    [SerializeField] AnimationCurve SunRotationX;
+    [SerializeField] AnimationCurve SunRotationY;
+    [SerializeField] AnimationCurve SunRotationZ;
+    [SerializeField] AnimationCurve AmbientLight;
     [SerializeField] float Speed=1;
     [SerializeField] List<Light> Lights;
     [SerializeField] float DayStart = 5.5f;
@@ -24,6 +30,10 @@ public class LightController : MonoBehaviour
                     if (localTimer > 24) localTimer = 0;
         if (localTimer > DayStart && localTimer<DayEnd) foreach (var item in Lights) item.enabled = true;
         else foreach (var item in Lights) item.enabled = true;
-            
+
+
+        Sun.intensity = SunIntensity.Evaluate(localTimer / 24f);
+        Sun.transform.rotation =Quaternion.Euler(SunRotationX.Evaluate(localTimer / 24f)*360, SunRotationY.Evaluate(localTimer / 24f) * 360, SunRotationZ.Evaluate(localTimer / 24f) * 360);
+        RenderSettings.ambientIntensity = AmbientLight.Evaluate(localTimer / 24f);
     }
 }
