7c4a839aa19da56ba722a0f2452c4252580faa40
Kos Kokos <eschota@gmail.com>
2020-12-16 00:10:37 +0700

    ебал выпадающие списки

--- Assets/LayersController.cs
+++ Assets/LayersController.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class LayersController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] TMPro.TMP_Dropdown drop;
+    
+    void Awake()
     {
-        
+        drop.onValueChanged.AddListener(OnChange);
     }
-
+    private void OnChange(int id)
+    {
+        WorldMapManager WM = FindObjectOfType<WorldMapManager>();
+        if (id == 0) WM.CurrentState = WM.CurrentState = WorldMapManager.State.Earth;
+        if (id == 1) WM.CurrentState = WM.CurrentState = WorldMapManager.State.Politic;
+        if (id == 2) WM.CurrentState = WM.CurrentState = WorldMapManager.State.Population;
+        if (id == 3) WM.CurrentState = WM.CurrentState = WorldMapManager.State.Science;
+        if (id == 4) WM.CurrentState = WM.CurrentState = WorldMapManager.State.Transport;
+        if (id == 5) WM.CurrentState = WM.CurrentState = WorldMapManager.State.Disaster;
+    }
+    private void OnDestroy()
+    {
+        drop.onValueChanged.RemoveListener(OnChange);
+    }
     // Update is called once per frame
     void Update()
     {
