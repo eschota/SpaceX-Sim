3ab4c39e309758d12ecbf8f4ea8f3f3440ebbc87
Vladislav <vladislav.lifirenko@gmai.com>
2020-12-19 11:02:35 +0200

    постройка в launch сцене (в процессе)

---
+++ Assets/BuildUnit/BuildCell.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+public class BuildCell : MonoBehaviour
+{
+    public int x, y;
+    
+    [SerializeField] private GameObject building;
+
+    private Renderer _renderer;
+    private bool _isSelected;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+    }
+
+    public void SetSelection(bool selected)
+    {
+        _isSelected = selected;
+        if (selected)
+            _renderer.material.SetColor("_Color", Color.green);
+        else
+            _renderer.material.SetColor("_Color", Color.white);
+    }
+    
+    public GameObject Building
+    {
+        get => building;
+        set => building = value;
+    }
+
+    public bool IsSelected => _isSelected;
+}
---
+++ Assets/BuildUnit/BuildingObject.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public class BuildingObject : MonoBehaviour
+{
+    [SerializeField] private int size;
+
+    public int Size => size;
+}
