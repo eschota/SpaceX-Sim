a7d8d89551915dfc003ac68b87681e960579c85b
eschota <69200550+eschota@users.noreply.github.com>
2020-12-21 18:40:07 +0700

    выбор модулей из списка доступных модулей

---
+++ Assets/UISelectModule.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UISelectModule : MonoBehaviour
+{
+    [SerializeField] Dropdown DropdownByTypes;
+    [SerializeField] CanvasGroup CG;
+    private Research _research;
+    public Research CurrentResearchSelected
+    {
+        get => _research;
+        set
+        {
+            _research = value;
+            if (value == null)
+            {
+                CG.alpha = 0;
+            }
+            else
+            { 
+                CG.alpha = 1;               
+            }
+        }
+    }
+
+   
+   
+}
