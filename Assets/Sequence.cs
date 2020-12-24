d30dabab9774c7858a4b66c4596c4a2287ad9786
eschota <69200550+eschota@users.noreply.github.com>
2020-12-25 00:49:43 +0700

    разделение сиквенсера по классам

--- Assets/Sequence.cs
+++ Assets/Sequence.cs
@@ -10,40 +10,19 @@ public class Sequence : MonoBehaviour
     [Header ("Управляет выбранным состоянием")]
     [SerializeField] public SequenceManager.State State;
 
-    [Header ("Кривая управления партикл системами")]
-
-    [SerializeField] public AnimationCurve ParticlesProcess;
-    [SerializeField] public List<ParticleSystem> PS;
 
 
-    private void Update()
+
+
+    public virtual void Update()
     {
         if (!TestWorking()) return;
-
-        ProcessParticles();
     }
-
-    void ProcessParticles()
-    {
-        foreach (var item in PS)
-        {
-            if (ParticlesProcess.Evaluate(manager.Timer) > 0f)
-            {
-               if (!item.isPlaying) item.Play();
-                Debug.Log("Play");
-            }
-            else
-            {
-                if (item.isPlaying) item.Stop();
-            }
-        }
-    }
-     bool TestWorking()
+    public bool TestWorking()
     {
         if (manager == null) return false;
         if (manager.CurrentState != State) return false;
-        if (PS == null) return false;
-        if (ParticlesProcess == null) return false;
         return true;
     }
+ 
 }
---
+++ Assets/SequenceParticles.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequenceParticles : Sequence
+{
+    [Header("Кривая управления партикл системами")]
+    [SerializeField] public AnimationCurve ParticlesProcess;
+    [SerializeField] public List<ParticleSystem> PS;
+
+
+
+    public override void Update()
+    {
+        base.Update();
+        ProcessParticles();
+    }
+void ProcessParticles()
+{
+        if (PS == null) return ;
+        if (ParticlesProcess == null) return;
+
+        foreach (var item in PS)
+    {
+        if (ParticlesProcess.Evaluate(manager.Timer) > 0f)
+        {
+            if (!item.isPlaying) item.Play();
+            Debug.Log("Play");
+        }
+        else
+        {
+            if (item.isPlaying) item.Stop();
+        }
+    }
+}
+ 
+}
