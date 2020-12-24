ee8918a7dbe90b14fdce4dfd358a8a6efd1e8dc6
eschota <69200550+eschota@users.noreply.github.com>
2020-12-24 20:12:40 +0700

    новая версия лайт контроллера

--- Assets/Engine/Managers/LightController.cs
+++ Assets/Engine/Managers/LightController.cs
@@ -1,42 +1,87 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 [ExecuteInEditMode]
 public class LightController : MonoBehaviour
 {
     [Range(0, 24)]
     [SerializeField] float localTimer;
+    [Header ("ШИ С ДЭ САН")]
     [SerializeField] Light Sun;
+    [Header("Яркость солнца в течении дня")]
     [SerializeField] AnimationCurve SunIntensity;
+    [Header("Ротейшен солнца в течении дня, значение домножается на 360")]
+
     [SerializeField] AnimationCurve SunRotationX;
     [SerializeField] AnimationCurve SunRotationY;
     [SerializeField] AnimationCurve SunRotationZ;
-    
+    [Header ("Яркость локальных ИС")]
     [SerializeField] AnimationCurve AllLight;
+    [Header("Яркость Эмбиент Отражений, Рефлекшн пробы" )]
     [SerializeField] AnimationCurve Reflections;
-    [SerializeField] AnimationCurve Ambients;
-    [SerializeField] float Speed=1;
-    [SerializeField] List<Light> Lights;
- 
-    [SerializeField] Material[] emissivMat;
-    [SerializeField] AnimationCurve EmissiveIntensity;
+    [Header("Яркость Эмбиент подсветки")]
 
+    [SerializeField] AnimationCurve Ambients;
+    [Header("Скорость времени")]
+
+    [SerializeField] float Speed=1;
+    [Header("Локальные лайты")]
+    [SerializeField] List<Light> Lights;
+    [Header("Самосветящиеся материалы с Emissive")]
+    [SerializeField] Material[] emissivMat;
+    [Header("Их яркость в течении дня ")]
+
+    [SerializeField] AnimationCurve EmissiveIntensity;
+    [Header("Рефлекшен пробы")]
     [SerializeField] List<ReflectionProbe> ReflectionProbes;
     Color[] colors;
-    
+    private bool isLocalLightController=false;
     List<float> RandomTimersForLights = new List<float>();
     
     private void Awake()
     {
-    if(Application.isPlaying)    if (FindObjectOfType<CameraInUnits>() == null) gameObject.AddComponent<CameraInUnits>();
+        IniTimer();
+        CorrectLocalTime();
+        CorrectPrefabsBuildingLocalTime();
+        StartRandomizeLights(); 
+    }
+    void Update()
+    {
+        if (isLocalLightController)// если это не глобальный контроллер света
+        {
+            ProcessTime();
+            ProcessLights();
+            ProcessEmissive();
+            ProcessReflectionProbes();
+        }
+        else
+        {
+            ProcessSun();
+            ProcessAmbientAndReflectionAmbient();
+        }
+
+        if (!Application.isPlaying&& Selection.activeGameObject==gameObject)// работа в эдиторе
+        {
+            List < LightController > LC = new List<LightController>(); LC.AddRange(FindObjectsOfType<LightController>());
+            foreach (var item in LC) item.localTimer = localTimer;
+            LC.Find(X => X.Sun != null).ProcessSun();
+            ProcessLights();
+            ProcessEmissive();
+            ProcessReflectionProbes();
+            ProcessAmbientAndReflectionAmbient();
+        }
+    }
+    private void IniTimer()
+    {
+        if (Application.isPlaying) if (FindObjectOfType<CameraInUnits>() == null) gameObject.AddComponent<CameraInUnits>();
         Time.timeScale = 1;
         if (TimeManager.Hours == null)
             localTimer = 5;
         else
             localTimer = TimeManager.Hours;
-        
-        CorrectLocalTime();
-        CorrectPrefabsBuildingLocalTime();
+    }
+    public void StartRandomizeLights()
+    {
         if (emissivMat != null)
         {
             colors = new Color[emissivMat.Length];
@@ -45,58 +90,76 @@ public class LightController : MonoBehaviour
                 colors[i] = emissivMat[i].GetColor("_EmissionColor");
             }
         }
-        if(Lights!=null)
-        for (int i = 0; i < Lights.Count; i++)
-        {
-            RandomTimersForLights.Add(Random.Range(-0.5f,0.5f));
-        }
-    }
-    
-    void Update()
-    {
-        if (Application.isPlaying)
-            localTimer += Time.deltaTime * Speed * TimeManager.TimeScale; 
-        if (localTimer > 24)
-            localTimer = 0;
         if (Lights != null)
-            for (int i = 0; i < Lights.Count; i++)             
-        {
-            float tempIntensity = AllLight.Evaluate((localTimer + RandomTimersForLights[i])/24f);
-            if (tempIntensity < 0.1f)
-                Lights[i].enabled = true;
-            else
-                Lights[i].enabled = false;
-        }
+            for (int i = 0; i < Lights.Count; i++)
+            {
+                RandomTimersForLights.Add(Random.Range(-0.5f, 0.5f));
+            }
+    }
 
-
-
+    void ProcessLights()
+    {
+        if (Lights != null)
+            for (int i = 0; i < Lights.Count; i++)
+            {
+                float tempIntensity = AllLight.Evaluate((localTimer + RandomTimersForLights[i]) / 24f);
+                if (tempIntensity < 0.1f)
+                    Lights[i].enabled = true;
+                else
+                    Lights[i].enabled = false;
+            }
+    }
+    void ProcessEmissive()
+    {
         if (emissivMat != null)
-            if(colors!=null)
-            for (int i = 0; i < colors.Length; i++)
-        {
-            emissivMat[i].SetColor("_EmissionColor", colors[i] * EmissiveIntensity.Evaluate(localTimer/24f));
-        }
-
-
+            if (colors != null)
+                for (int i = 0; i < colors.Length; i++)
+                {
+                    emissivMat[i].SetColor("_EmissionColor", colors[i] * EmissiveIntensity.Evaluate(localTimer / 24f));
+                }
+    }
+    void ProcessSun()
+    {
         if (Sun != null)
         {
             Sun.intensity = SunIntensity.Evaluate(localTimer / 24f);
             Sun.transform.rotation = Quaternion.Euler(SunRotationX.Evaluate(localTimer / 24f) * 360, SunRotationY.Evaluate(localTimer / 24f) * 360, SunRotationZ.Evaluate(localTimer / 24f) * 360);
         }
-        if(Ambients!=null)
-        RenderSettings.ambientIntensity = Ambients.Evaluate(localTimer / 24f);
+    }
+
+    void ProcessAmbientAndReflectionAmbient()
+    {
+        if (Ambients != null)
+            RenderSettings.ambientIntensity = Ambients.Evaluate(localTimer / 24f);
+        if (Reflections != null)
+            RenderSettings.reflectionIntensity = Reflections.Evaluate(localTimer / 24f);
+    }
+    void ProcessReflectionProbes()
+    {
         if (Reflections != null)
         {
-            RenderSettings.reflectionIntensity = Reflections.Evaluate(localTimer / 24f);
+           
             foreach (var item in ReflectionProbes) if (item != null) item.intensity = Reflections.Evaluate(localTimer / 24f);
         }
+    }
+   
+    void ProcessTime()
+    {
+        if (Application.isPlaying)
+            localTimer += Time.deltaTime * Speed * TimeManager.TimeScale;
+        if (localTimer > 24f)
+            localTimer = 0;
     }
     private void CorrectPrefabsBuildingLocalTime()
     {
         List <LightController> LC = new List<LightController>();
         LC.AddRange(FindObjectsOfType<LightController>()) ;
         LightController global = LC.Find(X => X.Sun != null);
-        if (global != null) localTimer = global.localTimer;
+        if (global != null)
+        {
+            isLocalLightController = true;
+            localTimer = global.localTimer;
+        }
     }
     private void CorrectLocalTime()
     {
