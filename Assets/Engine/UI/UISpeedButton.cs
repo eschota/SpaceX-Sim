14a83b916714da8732d772e63f79b56980a48cc4
eschota <69200550+eschota@users.noreply.github.com>
2021-01-04 22:16:23 +0700

    контроллер переключания времени в игре на кнопки гуя, клавы, умный возврат и всякое дерьмо

--- Assets/Engine/UI/UISpeedButton.cs
+++ Assets/Engine/UI/UISpeedButton.cs
@@ -8,17 +8,19 @@ public class UISpeedButton : MonoBehaviour
     [SerializeField] Vector3 ScaleDown= Vector3.one*0.75f;
     [SerializeField] Image play;
     [SerializeField] Image pause;
-
+    int lastid;
     private void Start()
     {
         for (int i = 1; i < transform.childCount; i++)
         {
             transform.GetChild(i).localScale = ScaleDown;
         }
-        pause.gameObject.SetActive(false);
+        lastid = 1;
     }
     public void click (int id)
     {
+
+        if (lastid != id && id!=0) lastid = id;
         for (int i = 0; i < transform.childCount; i++)
         {
             if (id == i) transform.GetChild(i).localScale = Vector3.one;
@@ -26,15 +28,34 @@ public class UISpeedButton : MonoBehaviour
 
             if (id == 0)
             {
-                Time.timeScale = 1;
+                Time.timeScale = 0;
             }if (id == 1)
             {
-                Time.timeScale = 30;
+                Time.timeScale = 1;
             }if (id == 2)
+            {
+                Time.timeScale = 30;
+            }if (id == 3)
             {
                 Time.timeScale = 100;
             }
         }
+
+
+        
     }
-     
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+            if (Time.timeScale <1) click(lastid);
+            else
+            {
+                
+                click(0);
+            }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1)) click(1);
+        if (Input.GetKeyDown(KeyCode.Alpha2)) click(2);
+        if (Input.GetKeyDown(KeyCode.Alpha3)) click(3);
+    }
 }
