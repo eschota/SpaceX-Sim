e4debb88bd8953232739144860f23dd0893f3c28
eschota <69200550+eschota@users.noreply.github.com>
2021-01-05 11:47:32 +0700

    UI отображения слоев ура

---
+++ Assets/WindowLayerInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindowLayerInfo : UIWindows
+{
+    public static WindowLayerInfo instance;
+
+    [SerializeField] public List<TMPro.TextMeshProUGUI> layersText;
+    void Awake() 
+    {
+        instance = this;
+    }
+
+    
+}
