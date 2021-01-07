f5f44fafb8cb52f76f11e875238e524b2fa467e4
eschota <69200550+eschota@users.noreply.github.com>
2021-01-07 15:06:00 +0700

    да будет это всё в гуе

--- Assets/WindowLayerInfo.cs
+++ Assets/WindowLayerInfo.cs
@@ -16,8 +16,8 @@ public class WindowLayerInfo : UIWindows
     {
 
         layersText[0].text = WorldMapManager.instance.CurrentHovered ? WorldMapManager.instance.CurrentHovered.Name : "No Country";
-        layersText[1].text = WorldMapManager.instance.GetPercentByTexture(WorldMapManager.instance.WorldLayers[1], WorldMapManager.instance.HoveredEarthUVCoord).ToString()+"%"; 
-        layersText[2].text = WorldMapManager.instance.GetPercentByTexture(WorldMapManager.instance.WorldLayers[2], WorldMapManager.instance.HoveredEarthUVCoord).ToString() + "%"; 
+        layersText[1].text = WorldMapManager.instance.CurrentHovered ?WorldMapManager.instance.CurrentHovered.Wealth +"$" : "N/A"; 
+        layersText[2].text = WorldMapManager.instance.CurrentHovered? WorldMapManager.instance.CurrentHovered.Population.ToString() + "K" : "N/A"; 
         layersText[3].text = WorldMapManager.instance.GetPercentByTexture(WorldMapManager.instance.WorldLayers[3], WorldMapManager.instance.HoveredEarthUVCoord).ToString() + "%"; 
         layersText[4].text = WorldMapManager.instance.GetPercentByTexture(WorldMapManager.instance.WorldLayers[4], WorldMapManager.instance.HoveredEarthUVCoord).ToString() + "%"; 
         layersText[5].text = WorldMapManager.instance.GetPercentByTexture(WorldMapManager.instance.WorldLayers[5], WorldMapManager.instance.HoveredEarthUVCoord).ToString() + "%";
