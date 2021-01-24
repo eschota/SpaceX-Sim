4c6bd924b4a2c61a5d97a3c9c11659e99fd248ad
Nikolls <Nikotino96@gmail.com>
2021-01-25 00:48:03 +0700

    - swing in the ass.

--- Assets/Engine/Visuals/ShakeRotate.cs
+++ Assets/Engine/Visuals/ShakeRotate.cs
@@ -9,18 +9,8 @@ public class ShakeRotate : MonoBehaviour
     [SerializeField]
     private float angle = 10f;
 
-    private float startRotation;
-    private float currentRotation;
-
-    private void Start()
-    {
-        startRotation = transform.localEulerAngles.y;
-    }
     void Update()
     {
-        currentRotation = angle / 2f * Mathf.Sin(Time.time * speed);
-        Vector3 euler = transform.localEulerAngles;
-        euler.y = startRotation + currentRotation;
-        transform.localEulerAngles = euler;
+        transform.Rotate(Vector3.up, Mathf.Sin(90 + Time.time * speed) * angle, Space.Self);
     }
 }
