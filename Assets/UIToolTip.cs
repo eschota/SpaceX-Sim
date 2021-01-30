adec64437b52efcf8d20d20b4ea06de50d567912
eschota <69200550+eschota@users.noreply.github.com>
2021-01-30 13:39:38 +0700

    тултипы

---
+++ Assets/UIToolTip.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIToolTip : UIWindows
+{
+    [SerializeField] public TMPro.TextMeshPro Name;
+    [SerializeField] public TMPro.TextMeshPro Description;
+    [SerializeField] public TMPro.TextMeshPro Cost;    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
