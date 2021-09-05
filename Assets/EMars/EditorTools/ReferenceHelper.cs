e489810ff374543f13c81a8386915c2a0e2456f6
eschota <69200550+eschota@users.noreply.github.com>
2021-09-06 02:59:42 +0700

    ебись оно всё конем пополам и четвертям!

--- Assets/EMars/EditorTools/ReferenceHelper.cs
+++ Assets/EMars/EditorTools/ReferenceHelper.cs
@@ -17,12 +17,13 @@ public class ReferenceHelper : MonoBehaviour
             Event e = new Event();
             while (Event.PopEvent(e))
             {
+            Vector3 lastPos = Camera.main.transform.position;
+            Image Ref1 = GameObject.Find("Ref1").GetComponent<Image>();
 
-                if (e.rawType == EventType.MouseDown && e.button == 0)
+            if (e.rawType == EventType.MouseDown && e.button == 0)
                 {
                 
-                Vector3 lastPos = Camera.main.transform.position;
-                    Image Ref1 = GameObject.Find("Ref1").GetComponent<Image>();
+                
                 if (!Ref1) return;
 
                 if (Ref1.color.a < 0.1f)
@@ -30,8 +31,7 @@ public class ReferenceHelper : MonoBehaviour
                     Ref1.color = TransparencyColor;
                 }
                 else Ref1.color = new Color(255, 255, 255, 0);
-                Camera.main.transform.Translate(Vector3.up,Space.World);
-                Camera.main.transform.position=lastPos;
+               
                 //Ray ray = Camera.main.ScreenPointToRay(new Vector2(e.mousePosition.x, Screen.height - e.mousePosition.y));
                 //RaycastHit hit;
                 //if (Physics.Raycast(ray, out hit))
@@ -46,6 +46,30 @@ public class ReferenceHelper : MonoBehaviour
 
                 //}
             }
+            if (e.rawType == EventType.MouseDrag && e.button == 1)
+            {
+                if (!Ref1) return;
+
+                Ref1.fillAmount = 1-(e.mousePosition.x / Screen.width);
+                Ref1.color = new Color(255,255,255,2* (1-(e.mousePosition.y / Screen.height)));
+
             }
+
+            if (e.rawType == EventType.MouseDown && e.button == 3)
+            {
+                Ref1.fillAmount = 0.1f;
+            }
+            if (e.rawType == EventType.Used && e.button ==2)
+            {
+                Ref1.fillAmount = 0.1f;
+            } 
+            if (e.type == EventType.ScrollWheel )
+            {
+                Ref1.fillAmount = 0.1f;
+            }
+
+                Camera.main.transform.Translate(Vector3.up, Space.World);
+            Camera.main.transform.position = lastPos;
+        }
         }
     }
