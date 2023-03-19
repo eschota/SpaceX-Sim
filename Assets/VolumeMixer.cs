1873ebd4902a7af01f356df535a16730c20e7258
eschota <eschota@gmail.com>
2023-03-20 00:51:36 +0300

    landing

---
+++ Assets/VolumeMixer.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+[ExecuteInEditMode]
+public class VolumeMixer : MonoBehaviour
+{
+    [SerializeField] Camera Cam;
+    [SerializeField] Volume Space;
+    [SerializeField] Volume Ground;
+    [SerializeField] Transform target;
+    [SerializeField] AnimationCurve curve;
+    [SerializeField] float startDist;
+
+
+    [SerializeField] Vector2 clipStart;
+    [Range (0,1)]
+    [SerializeField] double clc;
+    void Start()
+    {
+
+        startDist = Vector3.Distance(Cam.transform.position, target.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Application.isPlaying)
+        clc = (Vector3.Distance(Cam.transform.position, target.position)) / startDist;
+        //Cam.nearClipPlane = Mathf.Lerp(ClipStart.x, ClipEnd.x, Curve.Evaluate((float)clc));
+        Cam.farClipPlane = Mathf.Lerp(clipStart.x, clipStart.y, curve.Evaluate((float)clc));
+        //Space.weight = Mathf.Clamp01( curve.Evaluate((float) clc));
+        //Ground.weight =1- Space.weight;
+    }
+}
