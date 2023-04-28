a9ae7ae54d83dedd40dc4963af822c0e786d3e18
eschota <eschota@gmail.com>
2023-04-28 17:10:58 +0300

    planet rings

--- Assets/AmplifyShaderEditor/Plugins/Editor/Nodes/Master/MasterNodeDataCollector.cs
+++ Assets/AmplifyShaderEditor/Plugins/Editor/Nodes/Master/MasterNodeDataCollector.cs
@@ -130,6 +130,7 @@ namespace AmplifyShaderEditor
 		private List<PropertyDataCollector> m_aboveUsePassesList;
 		private List<PropertyDataCollector> m_belowUsePassesList;
 
+		private bool m_surfaceCustomShadowCaster = false;
 		private List<InputCoordsCollector> m_customShadowCoordsList;
 		private List<int> m_packSlotsList;
 		private string m_customAppDataItems;
@@ -163,6 +164,17 @@ namespace AmplifyShaderEditor
 		private Dictionary<string, InputCoordsCollector> m_customShadowCoordsDict;
 
 		private TextureChannelUsage[] m_requireTextureProperty = { TextureChannelUsage.Not_Used, TextureChannelUsage.Not_Used, TextureChannelUsage.Not_Used, TextureChannelUsage.Not_Used };
+		private WirePortDataType[] m_textureChannelSize =
+		{
+			WirePortDataType.FLOAT2,
+			WirePortDataType.FLOAT2,
+			WirePortDataType.FLOAT2,
+			WirePortDataType.FLOAT2,
+			WirePortDataType.FLOAT2,
+			WirePortDataType.FLOAT2,
+			WirePortDataType.FLOAT2,
+			WirePortDataType.FLOAT2
+		};
 
 		private bool m_dirtyAppData;
 		private bool m_dirtyInputs;
@@ -348,6 +360,27 @@ namespace AmplifyShaderEditor
 			m_vertexInterpDeclDict = new Dictionary<string, string>();
 
 			m_templateDataCollector = new TemplateDataCollector();
+		}
+
+		public void CopyTextureChannelSizeFrom( ref MasterNodeDataCollector dataCollector )
+		{
+			for( int i = 0 ; i < m_textureChannelSize.Length ; i++ )
+			{
+				SetTextureChannelSize( i , dataCollector.GetMaxTextureChannelSize( i ) );
+			}
+		}
+
+		public void SetTextureChannelSize( int channelIdx , WirePortDataType size )
+		{
+			if( size > m_textureChannelSize[ channelIdx ] )
+			{
+				m_textureChannelSize[ channelIdx ] = size;
+			}
+		}
+
+		public WirePortDataType GetMaxTextureChannelSize( int channelIdx )
+		{
+			return m_textureChannelSize[ channelIdx ];
 		}
 
 		public void SetChannelUsage( int channelId, TextureChannelUsage usage )
@@ -763,7 +796,7 @@ namespace AmplifyShaderEditor
 			if( list[ list.Count - 1 ].PropertyName.Contains( "[Header(" ) )
 			{
 				//Check if this is a complete property or just a standalone header
-				Match match = Regex.Match( list[ list.Count - 1 ].PropertyName, TemplateHelperFunctions.PropertiesPatternG );
+				Match match = Regex.Match( list[ list.Count - 1 ].PropertyName, TemplateHelperFunctions.PropertiesPatternI );
 				if( !match.Success )
 				{
 					list.RemoveAt( list.Count - 1 );
@@ -913,13 +946,6 @@ namespace AmplifyShaderEditor
 				m_dirtyUniforms = true;
 			}
 		}
-		public void AddFaceMacros()
-		{
-			for( int i = 0 ; i < Constants.FaceMacros.Length; i++ )
-			{
-				AddToDirectives( Constants.FaceMacros[ i ] );
-			}
-		}
 
 		public void AddASEMacros()
 		{
@@ -927,12 +953,6 @@ namespace AmplifyShaderEditor
 				return;
 
 			ParentGraph outsideGraph = UIUtils.CurrentWindow.OutsideGraph;
-#if !UNITY_2018_1_OR_NEWER
-			if( outsideGraph.IsStandardSurface && ( ( Using2DArrayMacrosMask & MacrosMask.AUTO ) == MacrosMask.AUTO || ( Using2DArrayMacrosMask & MacrosMask.LOD ) == MacrosMask.LOD ) )
-			{
-				return;
-			}
-#endif
 
 			//Debug.Log( UsingMacrosMask );
 			AddToDirectives( Constants.CustomASEStandarSamplingMacrosHelper[ 0 ], 1 );
@@ -964,30 +984,14 @@ namespace AmplifyShaderEditor
 			if( ( UsingCUBEMacrosMask & MacrosMask.GRAD ) == MacrosMask.GRAD )
 				AddToDirectives( Constants.CustomASEStandarSamplingMacrosRecent[ 11 ], 1 );
 
-#if !UNITY_2018_1_OR_NEWER
-			if( outsideGraph.IsStandardSurface )
-			{
-				//if( ( Using2DArrayMacrosMask & MacrosMask.AUTO ) == MacrosMask.AUTO )
-				//	AddToDirectives( Constants.CustomASEArraySamplingMacrosRecent[ 0 ], 1 );
-				//if( ( Using2DArrayMacrosMask & MacrosMask.LOD ) == MacrosMask.LOD )
-				//	AddToDirectives( Constants.CustomASEArraySamplingMacrosRecent[ 1 ], 1 );
-				if( ( Using2DArrayMacrosMask & MacrosMask.BIAS ) == MacrosMask.BIAS )
-					AddToDirectives( Constants.CustomASEArraySamplingMacrosRecent[ 2 ], 1 );
-				if( ( Using2DArrayMacrosMask & MacrosMask.GRAD ) == MacrosMask.GRAD )
-					AddToDirectives( Constants.CustomASEArraySamplingMacrosRecent[ 3 ], 1 );
-			} 
-			else
-#endif
-			{
-				if( ( Using2DArrayMacrosMask & MacrosMask.AUTO ) == MacrosMask.AUTO )
-					AddToDirectives( Constants.CustomASEStandarSamplingMacrosRecent[ 12 ], 1 );
-				if( ( Using2DArrayMacrosMask & MacrosMask.LOD ) == MacrosMask.LOD )
-					AddToDirectives( Constants.CustomASEStandarSamplingMacrosRecent[ 13 ], 1 );
-				if( ( Using2DArrayMacrosMask & MacrosMask.BIAS ) == MacrosMask.BIAS )
-					AddToDirectives( Constants.CustomASEStandarSamplingMacrosRecent[ 14 ], 1 );
-				if( ( Using2DArrayMacrosMask & MacrosMask.GRAD ) == MacrosMask.GRAD )
-					AddToDirectives( Constants.CustomASEStandarSamplingMacrosRecent[ 15 ], 1 );
-			}
+			if( ( Using2DArrayMacrosMask & MacrosMask.AUTO ) == MacrosMask.AUTO )
+				AddToDirectives( Constants.CustomASEStandarSamplingMacrosRecent[ 12 ], 1 );
+			if( ( Using2DArrayMacrosMask & MacrosMask.LOD ) == MacrosMask.LOD )
+				AddToDirectives( Constants.CustomASEStandarSamplingMacrosRecent[ 13 ], 1 );
+			if( ( Using2DArrayMacrosMask & MacrosMask.BIAS ) == MacrosMask.BIAS )
+				AddToDirectives( Constants.CustomASEStandarSamplingMacrosRecent[ 14 ], 1 );
+			if( ( Using2DArrayMacrosMask & MacrosMask.GRAD ) == MacrosMask.GRAD )
+				AddToDirectives( Constants.CustomASEStandarSamplingMacrosRecent[ 15 ], 1 );
 
 			AddToDirectives( Constants.CustomASEStandarSamplingMacrosHelper[ 1 ], 1 );
 
@@ -1018,30 +1022,14 @@ namespace AmplifyShaderEditor
 			if( ( UsingCUBEMacrosMask & MacrosMask.GRAD ) == MacrosMask.GRAD )
 				AddToDirectives( Constants.CustomASEStandarSamplingMacrosOlder[ 11 ], 1 );
 
-#if !UNITY_2018_1_OR_NEWER
-			if( outsideGraph.IsStandardSurface )
-			{
-				//if( ( Using2DArrayMacrosMask & MacrosMask.AUTO ) == MacrosMask.AUTO )
-				//	AddToDirectives( Constants.CustomASEArraySamplingMacrosOlder[ 0 ], 1 );
-				//if( ( Using2DArrayMacrosMask & MacrosMask.LOD ) == MacrosMask.LOD )
-				//	AddToDirectives( Constants.CustomASEArraySamplingMacrosOlder[ 1 ], 1 );
-				if( ( Using2DArrayMacrosMask & MacrosMask.BIAS ) == MacrosMask.BIAS )
-					AddToDirectives( Constants.CustomASEArraySamplingMacrosOlder[ 2 ], 1 );
-				if( ( Using2DArrayMacrosMask & MacrosMask.GRAD ) == MacrosMask.GRAD )
-					AddToDirectives( Constants.CustomASEArraySamplingMacrosOlder[ 3 ], 1 );
-			}
-			else
-#endif
-			{
-				if( ( Using2DArrayMacrosMask & MacrosMask.AUTO ) == MacrosMask.AUTO )
-					AddToDirectives( Constants.CustomASEStandarSamplingMacrosOlder[ 12 ], 1 );
-				if( ( Using2DArrayMacrosMask & MacrosMask.LOD ) == MacrosMask.LOD )
-					AddToDirectives( Constants.CustomASEStandarSamplingMacrosOlder[ 13 ], 1 );
-				if( ( Using2DArrayMacrosMask & MacrosMask.BIAS ) == MacrosMask.BIAS )
-					AddToDirectives( Constants.CustomASEStandarSamplingMacrosOlder[ 14 ], 1 );
-				if( ( Using2DArrayMacrosMask & MacrosMask.GRAD ) == MacrosMask.GRAD )
-					AddToDirectives( Constants.CustomASEStandarSamplingMacrosOlder[ 15 ], 1 );
-			}
+			if( ( Using2DArrayMacrosMask & MacrosMask.AUTO ) == MacrosMask.AUTO )
+				AddToDirectives( Constants.CustomASEStandarSamplingMacrosOlder[ 12 ], 1 );
+			if( ( Using2DArrayMacrosMask & MacrosMask.LOD ) == MacrosMask.LOD )
+				AddToDirectives( Constants.CustomASEStandarSamplingMacrosOlder[ 13 ], 1 );
+			if( ( Using2DArrayMacrosMask & MacrosMask.BIAS ) == MacrosMask.BIAS )
+				AddToDirectives( Constants.CustomASEStandarSamplingMacrosOlder[ 14 ], 1 );
+			if( ( Using2DArrayMacrosMask & MacrosMask.GRAD ) == MacrosMask.GRAD )
+				AddToDirectives( Constants.CustomASEStandarSamplingMacrosOlder[ 15 ], 1 );
 
 			AddToDirectives( Constants.CustomASEStandarSamplingMacrosHelper[ 2 ], 1 );
 		}
@@ -2053,7 +2041,7 @@ namespace AmplifyShaderEditor
 
 		public bool IsTemplate { get { return m_masterNodeCategory == AvailableShaderTypes.Template; } }
 
-		public bool IsSRP { get { return ( TemplateDataCollectorInstance.CurrentSRPType == TemplateSRPType.Lightweight || TemplateDataCollectorInstance.CurrentSRPType == TemplateSRPType.HD ); } }
+		public bool IsSRP { get { return ( TemplateDataCollectorInstance.CurrentSRPType == TemplateSRPType.URP || TemplateDataCollectorInstance.CurrentSRPType == TemplateSRPType.HDRP ); } }
 
 		public AvailableShaderTypes MasterNodeCategory
 		{
@@ -2254,6 +2242,13 @@ namespace AmplifyShaderEditor
 			}
 		}
 
+		public bool SurfaceCustomShadowCaster
+		{
+			get { return m_surfaceCustomShadowCaster; }
+			set { m_surfaceCustomShadowCaster = value; }
+		}
+
+		public bool CustomOutline { get { return UsingCustomOutlineColor || UsingCustomOutlineWidth || UsingCustomOutlineAlpha; } }
 		public List<PropertyDataCollector> InputList { get { return m_inputList; } }
 		public List<PropertyDataCollector> CustomInputList { get { return m_customInputList; } }
 		public List<PropertyDataCollector> PropertiesList { get { return m_propertiesList; } }
@@ -2302,7 +2297,7 @@ namespace AmplifyShaderEditor
 				if( IsTemplate )
 					return m_templateDataCollector.CurrentSRPType;
 
-				return TemplateSRPType.BuiltIn;
+				return TemplateSRPType.BiRP;
 			}
 		}
 
